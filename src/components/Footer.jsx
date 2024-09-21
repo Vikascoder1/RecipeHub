ae5b8cf00e716b22f37eee4eeee5a439e04707ff
vikas <vikaschaurasiya971@gmail.com>
2024-09-21 13:06:45 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <footer>
+    <p>Created By <a href="" target='_blank'>Vikas Chaurasiya</a></p>
+   </footer>
+  )
+}
+
+export default Footer
